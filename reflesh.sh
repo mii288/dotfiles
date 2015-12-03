471513e73bcc146db521e70171e7286c4856e795
mii288 <atteeen@yahoo.co.jp>
2015-12-03 10:14:14 +0900

    refresh.sh追加

---
+++ reflesh.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+ln -sf ~/dotfiles ~/.bashrc
+ln -sf ~/dotfiles ~/.ctags
+ln -sf ~/dotfiles ~/.git
+ln -sf ~/dotfiles ~/.git-completion.bash
+ln -sf ~/dotfiles ~/.git-prompt.sh
+ln -sf ~/dotfiles ~/.gitconfig
+ln -sf ~/dotfiles ~/.gitignore
+ln -sf ~/dotfiles ~/.tmux.conf
+ln -sf ~/dotfiles ~/.vimrc
