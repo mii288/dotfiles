9cfed62e2a47651d10eb9c52bc394740af3021dc
mii288 <atteeen@yahoo.co.jp>
2015-12-03 10:51:27 +0900

    zprezto系設定ファイルの追加

--- reflesh.sh
+++ reflesh.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
-ln -sf ~/dotfiles ~/.bashrc
-ln -sf ~/dotfiles ~/.ctags
-ln -sf ~/dotfiles ~/.git
-ln -sf ~/dotfiles ~/.git-completion.bash
-ln -sf ~/dotfiles ~/.git-prompt.sh
-ln -sf ~/dotfiles ~/.gitconfig
-ln -sf ~/dotfiles ~/.gitignore
-ln -sf ~/dotfiles ~/.tmux.conf
-ln -sf ~/dotfiles ~/.vimrc
+ln -sf ~/dotfiles/.bashrc              $HOME
+ln -sf ~/dotfiles/.ctags               $HOME
+ln -sf ~/dotfiles/.git-completion.bash $HOME
+ln -sf ~/dotfiles/.git-prompt.sh       $HOME
+ln -sf ~/dotfiles/.gitconfig           $HOME
+ln -sf ~/dotfiles/.gitignore           $HOME
+ln -sf ~/dotfiles/.tmux.conf           $HOME
+ln -sf ~/dotfiles/.vimrc               $HOME
+
+ln -si ~/dotfiles/zlogout              $HOME/.zlogout
+ln -si ~/dotfiles/zpreztorc            $HOME/.zpreztorc
+ln -si ~/dotfiles/zprofile             $HOME/.zprofile
+ln -si ~/dotfiles/zshenv               $HOME/.zshenv
+ln -si ~/dotfiles/zshrc                $HOME/.zshrc
