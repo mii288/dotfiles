abcd94683326ca887dd53c6087086a07914c8ddb
稲置 美里 <inaoki.misato@willgate.co.jp>
2018-09-24 17:31:37 +0900

    :bug: パス修正

--- reflesh.sh
+++ reflesh.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.bashrc              $HOME
 ln -sf ~/dotfiles/.ctags               $HOME
-ln -sf ~/dotfiles/.gitconfig           $HOME
+ln -sf ~/dotfiles/git/.gitconfig       $HOME
 ln -sf ~/dotfiles/.tmux.conf           $HOME
 ln -sf ~/dotfiles/.vimrc               $HOME
 ln -sf ~/dotfiles/.vim                 $HOME/.config/nvim/
