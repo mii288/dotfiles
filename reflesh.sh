b91ea4092baaf3a6d83d2b6cc4c9006a20006cba
mii288 <atteeen@gmail.com>
2018-10-02 17:52:30 +0900

    :fire: 存在しないファイルについての記述を削除

--- reflesh.sh
+++ reflesh.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.ctags               $HOME
-ln -sf ~/dotfiles/git/.gitconfig       $HOME
 ln -sf ~/dotfiles/.tmux.conf           $HOME
 ln -sf ~/dotfiles/vim/.vimrc   $HOME
 ln -sf ~/dotfiles/vim/.vim     $HOME/.config/nvim/
