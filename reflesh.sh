60b5165963fcefd9e5ed626b22c7ce7c7b988fb2
mii288 <atteeen@gmail.com>
2018-10-03 00:01:04 +0900

    :truck: `.tmux.conf`を`tmux`ディレクトリに移動

--- reflesh.sh
+++ reflesh.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.ctags       $HOME
-ln -sf ~/dotfiles/.tmux.conf   $HOME
-ln -sf ~/dotfiles/vim/vimrc    $HOME/.vim
-ln -sf ~/dotfiles/vim/.vim     $HOME/.config/nvim/
-ln -sf ~/dotfiles/vim/.vimrc   $HOME/.config/nvim/init.vim
-ln -sf ~/dotfiles/git          $HOME/.config
-ln -sf ~/dotfiles/fish         $HOME/.config
+ln -sf ~/dotfiles/.ctags          $HOME
+ln -sf ~/dotfiles/tmux/.tmux.conf $HOME
+ln -sf ~/dotfiles/vim/vimrc       $HOME/.vim
+ln -sf ~/dotfiles/vim/.vim        $HOME/.config/nvim/
+ln -sf ~/dotfiles/vim/.vimrc      $HOME/.config/nvim/init.vim
+ln -sf ~/dotfiles/git             $HOME/.config
+ln -sf ~/dotfiles/fish            $HOME/.config
