b3f4bfcab5cb9b36612046aa443ca8d560e5253f
mii288 <atteeen@gmail.com>
2018-10-02 19:08:27 +0900

    :track: vim関連の設定ファイルを移動

--- reflesh.sh
+++ reflesh.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.ctags       $HOME
 ln -sf ~/dotfiles/.tmux.conf   $HOME
-ln -sf ~/dotfiles/vim/.vimrc   $HOME
+ln -sf ~/dotfiles/vim/vimrc    $HOME/.vim
 ln -sf ~/dotfiles/vim/.vim     $HOME/.config/nvim/
 ln -sf ~/dotfiles/vim/.vimrc   $HOME/.config/nvim/init.vim
 ln -sf ~/dotfiles/git          $HOME/.config
