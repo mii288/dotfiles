ea8b06f263c24bfaf116d00fc0de2c2d0131b814
mii288 <atteeen@gmail.com>
2020-06-13 13:55:50 +0900

    feat: Support to work outside of $HOME

--- reflesh.sh
+++ reflesh.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.ctags          $HOME
-ln -sf ~/dotfiles/tmux/.tmux.conf $HOME
-ln -sf ~/dotfiles/vim/vimrc       $HOME/.vim
-ln -sf ~/dotfiles/vim/.vim        $HOME/.config/nvim/
-ln -sf ~/dotfiles/vim/.vimrc      $HOME/.config/nvim/init.vim
-ln -sf ~/dotfiles/git             $HOME/.config
-ln -sf ~/dotfiles/fish            $HOME/.config
+ln -sf $(pwd)/.ctags          $HOME
+ln -sf $(pwd)/tmux/.tmux.conf $HOME
+ln -sf $(pwd)/vim/vimrc       $HOME/.vim
+ln -sf $(pwd)/vim/            $HOME/.config
+ln -sf $(pwd)/git             $HOME/.config
+ln -sf $(pwd)/fish            $HOME/.config
