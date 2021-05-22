fcf30078f6531ccb92ea7ef15ce5a29de32965c0
mii288 <atteeen@gmail.com>
2021-05-23 00:50:21 +0900

    fix: change symblic link path

--- reflesh.sh
+++ reflesh.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-ln -sf $(pwd)/.ctags          $HOME
+mkdir $HOME/.config
+ln -sf $(pwd)/.ctags $HOME
 ln -sf $(pwd)/tmux/.tmux.conf $HOME
-ln -sf $(pwd)/vim/vimrc       $HOME/.vim
-ln -sf $(pwd)/vim/            $HOME/.config
-ln -sf $(pwd)/git             $HOME/.config
-ln -sf $(pwd)/fish            $HOME/.config
+ln -sf $(pwd)/vim/vimrc $HOME/.vim
+ln -sf $(pwd)/vim $HOME/.config/vim 
+ln -sf $(pwd)/git $HOME/.config/git 
+ln -sf $(pwd)/fish $HOME/.config/fish
