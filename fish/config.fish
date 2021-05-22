80ef79805faf4c651014599624201c3674da5bed
mii288 <atteeen@gmail.com>
2021-05-23 00:36:38 +0900

    feat(fish): use history-search-backward

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,14 @@
 alias repo="peco_ghq_project"
+
+# down-or-search	
+## Down Arrow
+bind -k down history-prefix-search-forward
+bind \e\[B history-prefix-search-forward
+## CTRL - n
+bind \cn history-prefix-search-forward
+# up-or-search	
+## Up Arrow
+bind -k up history-prefix-search-backward
+bind \e\[A history-prefix-search-backward
+## CTRL - p
+bind \cp history-prefix-search-backward
