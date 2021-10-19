534d753548b31dff5dc5159c6618bd8bb15a0394
mii288 <mii288@users.noreply.github.com>
2021-10-19 19:13:34 +0900

    fix(vim): fix .vimrc path

--- reflesh.sh
+++ reflesh.sh
@@ -4,7 +4,7 @@ mkdir -p $HOME/.config/nvim
 ln -sfn $(pwd)/.ctags $HOME
 ln -sfn $(pwd)/tmux/.tmux.conf $HOME
 ln -sfn $(pwd)/vim/vimrc $HOME/.config/nvim/init.vim
-ln -sfn $(pwd)/vim/vimrc $HOME/.vim
+ln -sfn $(pwd)/vim/vimrc $HOME/.vimrc
 ln -sfn $(pwd)/vim $HOME/.config/vim
 ln -sfn $(pwd)/git $HOME/.config/git
 ln -sfn $(pwd)/fish $HOME/.config/fish
