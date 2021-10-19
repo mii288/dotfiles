30e973b8848644c0ed3c62d0cbc45f35c137d32e
mii288 <atteeen@gmail.com>
2021-10-19 18:59:01 +0900

    feat(fish): uninstall pyenv

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -3,5 +3,3 @@ fish_add_path /usr/local/bin
 
 status --is-interactive; and source (nodenv init -|psub)
 status --is-interactive; and source (rbenv init -|psub)
-status --is-interactive; and source (pyenv init -|psub)
-status --is-interactive; and source (pyenv virtualenv-init -|psub)
