f2345e0ba0634894c4b2ef1fd371254f0bae014a
mii288 <mii288@users.noreply.github.com>
2021-11-03 01:31:34 +0900

    feat(fish): hide peco_git_rebase_interactive error

--- fish/conf.d/peco_git_rebase_interactive.fish
+++ fish/conf.d/peco_git_rebase_interactive.fish
@@ -1,6 +1,6 @@
 function peco_git_rebase_interactive
-  git log --oneline \
-  | peco \
-  | cut -d " " -f 1 \
-  | xargs -I @ git rebase -i "@"
+    set -l hash (git log --oneline peco cut -d " " -f 1)
+    if test $hash
+        git rebase -i "$hash"
+    end
 end
