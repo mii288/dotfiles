19365550cae2ff81e76d597d5d97e7cdd7c799f7
mii288 <mii288@users.noreply.github.com>
2021-11-16 12:48:33 +0900

    Revert "feat(fish): hide peco_git_rebase_interactive error"
    This reverts commit f2345e0ba0634894c4b2ef1fd371254f0bae014a.

--- fish/conf.d/peco_git_rebase_interactive.fish
+++ fish/conf.d/peco_git_rebase_interactive.fish
@@ -1,6 +1,6 @@
 function peco_git_rebase_interactive
-    set -l hash (git log --oneline peco cut -d " " -f 1)
-    if test $hash
-        git rebase -i "$hash"
-    end
+  git log --oneline \
+  | peco \
+  | cut -d " " -f 1 \
+  | xargs -I @ git rebase -i "@"
 end
