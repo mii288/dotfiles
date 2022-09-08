18c52bad66dc2ad558d561e81d263c80a751a6e1
mii288 <mii288@users.noreply.github.com>
2022-09-09 02:59:30 +0900

    fix: Add brew initialize

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,5 +1,7 @@
 fish_add_path /usr/local/sbin
 fish_add_path /usr/local/bin
 
+eval (/opt/homebrew/bin/brew shellenv)
+
 status --is-interactive; and source (nodenv init -|psub)
 eval (direnv hook fish)
