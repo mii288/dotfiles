a22abe9ab43620a304f995a0e37be1bc3ad50d3f
mii288 <mii288@users.noreply.github.com>
2022-12-10 15:06:28 +0000

    fix: WSL環境下でも動くように修正

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,7 +1,13 @@
 fish_add_path /usr/local/sbin
 fish_add_path /usr/local/bin
 
-eval (/opt/homebrew/bin/brew shellenv)
+if test -d "/opt/homebrew/bin/brew"
+  eval (/opt/homebrew/bin/brew shellenv)
+end
+
+if test -d "/home/linuxbrew/.linuxbrew/bin/brew" 
+  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+end
 
 status --is-interactive; and source (nodenv init -|psub)
 eval (direnv hook fish)
