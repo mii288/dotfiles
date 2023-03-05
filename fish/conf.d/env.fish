7abd3f0725cabf269b899f3be575d992536f6980
misato-inaoki <misato.inaoki@linecorp.com>
2023-03-05 11:48:07 +0900

    fix(nodenv): set fish_user_paths

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -4,4 +4,6 @@ fish_add_path /usr/local/bin
 eval (/opt/homebrew/bin/brew shellenv)
 
 status --is-interactive; and source (nodenv init -|psub)
+set -Ux fish_user_paths $HOME/.nodenv/bin $fish_user_paths
+
 eval (direnv hook fish)
