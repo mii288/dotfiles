b15bf7a550f915ab6e13e16c1d52aa4df07a3e9a
mii288 <mii288@users.noreply.github.com>
2023-04-17 23:31:34 +0900

    feat(alias): vi = vim

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -2,5 +2,6 @@ alias repo="peco_ghq_project"
 alias cat="bat"
 alias ls="exa --icons"
 alias moonmoji="emoji-clock"
+alias vi="vim"
 alias vim="nvim"
 alias ggpull='git pull origin (git current-branch)'
