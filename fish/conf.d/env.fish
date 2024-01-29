bc21bc08198aa09889334532eb733163767f4435
mii288 <mii288@users.noreply.github.com>
2024-01-29 17:39:26 +0900

    chore(fish): update env.fish

--- fish/conf.d/env.fish
+++ fish/conf.d/env.fish
@@ -1,15 +1,20 @@
 fish_add_path /usr/local/sbin
 fish_add_path /usr/local/bin
 
-if test -e "/opt/homebrew/bin/brew"
-  eval (/opt/homebrew/bin/brew shellenv)
+# brew
+if test -e /opt/homebrew/bin/brew
+    eval (/opt/homebrew/bin/brew shellenv)
 end
 
-if test -e "/home/linuxbrew/.linuxbrew/bin/brew" 
-  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+if test -e /home/linuxbrew/.linuxbrew/bin/brew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
 
+# nodenv
 status --is-interactive; and source (nodenv init -|psub)
 set -Ux fish_user_paths $HOME/.nodenv/bin $fish_user_paths
 
-eval (direnv hook fish)
+# direnv
+if direnv -v &>/dev/null
+    eval (direnv hook bash)
+end
